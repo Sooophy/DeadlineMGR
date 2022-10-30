46295018e75b370288b1b0d2ecc44c0a08baadf1
motianjun4 <motianjun4@gmail.com>
2022-10-29 20:27:29 -0400

    Initial Commit

---
+++ DeadlineMgr/DeadlineMgrApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeadlineMgrApp.swift
+//  DeadlineMgr
+//
+//  Created by Tianjun Mo on 2022/10/29.
+//
+
+import SwiftUI
+
+@main
+struct DeadlineMgrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
