72f9c56f1094e88087de251ac66fda46e5d4d9d3
ys331@duke.edu <ys331@duke.edu>
2022-11-15 10:53:42 -0500

    Adjust the timeline view to enable date picking

---
+++ DeadlineMgr/Views/Timeline/DateHolder.swift
@@ -0,0 +1,12 @@
+//
+//  DateHolder.swift
+//  DeadlineMgr
+//
+//  Created by Sophie on 11/15/22.
+//
+
+import Foundation
+
+class DateHolder : ObservableObject {
+    @Published var date = Date()
+}
