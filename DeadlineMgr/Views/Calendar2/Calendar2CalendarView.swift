2969cf590a99baee33457f88e46909299374feab
motianjun4 <motianjun4@gmail.com>
2022-11-15 00:33:44 -0500

    add: Calendar2View details

--- DeadlineMgr/Views/Calendar2/Calendar2CalendarView.swift
+++ DeadlineMgr/Views/Calendar2/Calendar2CalendarView.swift
@@ -10,9 +10,101 @@ import Foundation
 import SwiftUI
 
 struct Calendar2CalendarView: View, UIViewRepresentable {
-    func makeUIView(context: Context) -> DayView {
-        DayViewController().dayView
+    @Binding var date: Date
+    var events: [Event]
+    var ckEvents: [CalendarKit.EventDescriptor] {
+        events.filter {
+            event in
+            Calendar.current.isDate(date, inSameDayAs: event.dueAt)
+        }.map { event in
+            let ckEvent = CalendarKit.Event()
+            ckEvent.dateInterval = DateInterval(start: event.dueAt, duration: TimeInterval(60 * 24))
+            ckEvent.text = event.title
+            return ckEvent
+        }
     }
 
-    func updateUIView(_ uiView: DayView, context: Context) {}
+    var dataSource: DataSource?
+    
+    init(date: Binding<Date>, events: [Event]) {
+        self._date = date
+        self.events = events
+        self.dataSource = DataSource(events: events)
+    }
+
+    func makeUIView(context: Context) -> DayView {
+        let dayView = DayViewController().dayView
+        dayView.delegate = context.coordinator
+        dayView.dataSource = dataSource
+        dayView.reloadData()
+        
+        return dayView
+    }
+    
+    class DataSource: EventDataSource {
+        var events: [Event]
+        init(events: [Event]) {
+            self.events = events
+        }
+
+        func eventsForDate(_ date: Date) -> [CalendarKit.EventDescriptor] {
+            let mapped = events.filter {
+                event in
+                Calendar.current.isDate(date, inSameDayAs: event.dueAt)
+            }.map { event in
+                let ckEvent = CalendarKit.Event()
+                ckEvent.dateInterval = DateInterval(start: event.dueAt.addingTimeInterval(TimeInterval(-3600)), duration: TimeInterval(3600))
+                ckEvent.text = event.title
+                ckEvent.color = UIColor(event.color)
+                return ckEvent
+            }
+            
+            return mapped
+        }
+    }
+
+    func updateUIView(_ uiView: DayView, context: Context) {
+        uiView.move(to: date)
+        uiView.reloadData()
+    }
+    
+    final class Coordinator: NSObject, DayViewDelegate {
+        @Binding var date: Date
+        
+        init(date: Binding<Date>) {
+            self._date = date
+        }
+        
+        func dayViewDidLongPressEventView(_ eventView: CalendarKit.EventView) {}
+        
+        func dayView(dayView: CalendarKit.DayView, didTapTimelineAt date: Date) {}
+        
+        func dayView(dayView: CalendarKit.DayView, didLongPressTimelineAt date: Date) {}
+        
+        func dayViewDidBeginDragging(dayView: CalendarKit.DayView) {}
+        
+        func dayViewDidTransitionCancel(dayView: CalendarKit.DayView) {}
+        
+        func dayView(dayView: CalendarKit.DayView, willMoveTo date: Date) {}
+        
+        func dayView(dayView: CalendarKit.DayView, didMoveTo date: Date) {
+            self.date = date
+            dayView.reloadData()
+        }
+        
+        func dayView(dayView: CalendarKit.DayView, didUpdate event: CalendarKit.EventDescriptor) {}
+        
+        func dayViewDidSelectEventView(_ eventView: EventView) {}
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(date: $date)
+    }
+}
+
+extension DayViewController {
+    func eventsForDate(_ date: Date) -> [EventDescriptor] {
+        print("evnets")
+        return []
+    }
 }
--- DeadlineMgr/Views/Calendar2/Calendar2View.swift
+++ DeadlineMgr/Views/Calendar2/Calendar2View.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct Calendar2View: View {
+    @EnvironmentObject var modelData: ModelData
+
+    @State var selected_date: Date = .now
+    @State var is_date_presented: Bool = false
+
+    var events: [Event] {
+        modelData.dataBase.values.map { event in
+            event
+        }
+    }
+
     var body: some View {
-        Calendar2CalendarView()
+        NavigationView {
+            ZStack {
+                Calendar2CalendarView(date: $selected_date, events: events)
+//                Text(String(describing: events))
+
+            }.navigationBarTitle("Calendar", displayMode: .inline).toolbar {
+                ToolbarItemGroup(placement: .navigationBarLeading) {
+                    Button {
+                        is_date_presented = true
+                    } label: {
+                        Image(systemName: "calendar")
+                    }
+                }
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button("Today") {
+                        selected_date = .now
+                    }
+                }
+            }
+        }.sheet(isPresented: $is_date_presented) {
+            DatePicker(
+                selection: $selected_date,
+                displayedComponents: [.date]
+            ) {
+                Text("date")
+            }
+            .datePickerStyle(.graphical)
+//            .frame(height: 330)
+            .presentationDetents([.height(400)])
+        }
     }
 }
 
 struct Calendar2View_Previews: PreviewProvider {
     static var previews: some View {
-        Calendar2View()
+        Calendar2View().environmentObject(ModelData())
     }
 }
